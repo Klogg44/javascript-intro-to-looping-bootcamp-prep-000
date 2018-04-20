b12306eaca1baf1942788175f52efa5bcf7a2e3c
Spencer Croy <spencerccroy@gmail.com>
2018-04-20 00:47:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,12 @@ function whileLoop(number) {
   return 'done'
 }
 
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.25
+  }
+  
+  do {
+    
+  } while (array.length . 0 && maybeTrue())
+}
